c7010bde15205ecf6b6db62c7f62609f8d6a14e0
derickard <derickard@gmail.com>
2020-05-28 15:38:07 -0400

    Studio finished

---
+++ AreaOfCircle/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaOfCircle
+{
+    class Circle
+    {
+        public double radius;
+        public double area;
+        public double circumference;
+        public double diameter;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            this.area = Math.PI * Math.Pow(radius, 2);
+            this.circumference = Math.PI * 2 * radius;
+            this.diameter = 2 * radius;
+        }
+    }
+}
---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = -1;
+            double mpg;
+            do
+            {
+                try
+                {
+                    Console.Write("Enter a radius: ");
+                    radius = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Radius must be a number!");
+                }
+            } while (radius < 0);
+
+            Circle myCircle = new Circle(radius);
+
+            Console.WriteLine($"The circle of radius {myCircle.radius} has:\nArea: {myCircle.area}\nCircumference: {myCircle.circumference}\nDiameter: {myCircle.diameter}");
+
+            Console.Write("\nWhat is your car's mph?: ");
+            mpg = double.Parse(Console.ReadLine());
+            Console.WriteLine($"With a gas milegae of {mpg}, you need {myCircle.circumference / mpg} gallons of gas to drive around this circle.");
+
+
+
+
+        }
+    }
+}
